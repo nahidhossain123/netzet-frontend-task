c5923d9f2c0fa011bce59f6c92c043f33a3389b8
Nahid Hossain <nahidhossain351@gmail.com>
2025-04-17 13:31:06 +0600

    feat: improve mobile layout for discount promo

--- app/components/DiscountBanner.tsx
+++ app/components/DiscountBanner.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const DiscountBanner = () => {
     return (
-        <div className="bg-gradient-to-r from-[#FC004E] to-[#10CBE0]">
-            <p className="py-2.5 font-extrabold text-[22px] text-center">
-                <span className="text-[#10CBE0]">🚀 FRESH BEGINNINGS SALE:</span>
+        <div className="bg-gradient-to-r from-brand-primary to-brand-secondary font-figtree">
+            <p className="p-4 md:py-2.5 font-extrabold text-4 sm:text-[22px] text-center">
+                <span className="text-brand-secondary">🚀 FRESH BEGINNINGS SALE:</span>
                 <span className="text-white"> Extra 25% OFF, Limited Spots </span>
                 <span className="text-white">- start your journey today!</span>
             </p>
