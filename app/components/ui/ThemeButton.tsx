f345ee6ff62df4125dc2a5011bebd55d2848cb1c
Nahid Hossain <nahidhossain351@gmail.com>
2025-04-19 00:24:24 +0600

    update(button): improve Button component with smarter icon handling

--- app/components/ui/ThemeButton.tsx
+++ app/components/ui/ThemeButton.tsx
@@ -1,14 +1,52 @@
+import Image from 'next/image'
 import React, { ReactNode } from 'react'
 
 interface ThemeButtonPropsType {
     children: ReactNode,
     buttonStyle?: string,
+    onButtonClick?: () => void,
+    iconSrc?: string,
+    iconAlt?: string,
+    iconStyle?: string,
+    iconWidth?: number,
+    iconHeight?: number
 }
 
-const ThemeButton = ({ children, buttonStyle }: ThemeButtonPropsType) => {
+const ThemeButton = ({
+    children,
+    buttonStyle,
+    onButtonClick,
+    iconSrc,
+    iconAlt,
+    iconStyle,
+    iconWidth,
+    iconHeight
+}: ThemeButtonPropsType) => {
     return (
-        <button className={`drop-shadow-md drop-shadow-brand-secondary50 bg-brand-primary py-2 px-[83px] rounded-[10px] text-white text-4 font-bold border-2 border-brand-primary hover:bg-transparent hover:border-white hover:scale-110 transition-all  ${buttonStyle && buttonStyle}`}>
-            {children}
+        <button onClick={onButtonClick} className={`
+            w-full 
+            md:w-[unset] 
+            flex 
+            items-center
+            justify-center
+            gap-2.5
+            drop-shadow-md 
+            drop-shadow-brand-secondary50 
+            bg-brand-primary 
+            py-2 
+            px-[83px]
+            rounded-[10px]
+            text-white
+            text-4 
+            font-bold 
+            border-2 
+            border-brand-primary 
+            hover:bg-transparent 
+            hover:border-white 
+            ${buttonStyle && buttonStyle}
+        `}>
+            <span>{children}</span>
+            {iconSrc && (<img src={iconSrc} className={`${iconStyle}`} alt={iconAlt ? iconAlt : 'icon'} />)}
         </button>
     )
 }
