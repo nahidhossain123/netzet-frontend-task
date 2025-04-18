89b8249a526b58955e9055ffe86f19a2a8a300e7
Nahid Hossain <nahidhossain351@gmail.com>
2025-04-18 10:53:47 +0600

    feat(colors): add multiple secondary color variants

--- app/components/DiscountBanner.tsx
+++ app/components/DiscountBanner.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const DiscountBanner = () => {
     return (
-        <div className="bg-gradient-to-r from-brand-primary to-brand-secondary font-figtree">
+        <div className="bg-gradient-to-r from-brand-primary to-brand-secondary100 font-figtree">
             <p className="p-4 md:py-2.5 font-extrabold text-4 sm:text-[22px] text-center">
-                <span className="text-brand-secondary">🚀 FRESH BEGINNINGS SALE:</span>
+                <span className="text-brand-secondary100">🚀 FRESH BEGINNINGS SALE:</span>
                 <span className="text-white"> Extra 25% OFF, Limited Spots </span>
                 <span className="text-white">- start your journey today!</span>
             </p>
