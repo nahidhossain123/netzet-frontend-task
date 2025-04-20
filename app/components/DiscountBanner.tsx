532b2469e686a1978de0488d0e6cbcebce1a736d
Nahid Hossain <nahidhossain351@gmail.com>
2025-04-20 11:08:53 +0600

    update: dicount section responsive width

--- app/components/DiscountBanner.tsx
+++ app/components/DiscountBanner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DiscountBanner = () => {
     return (
-        <div className="max-w-[1440px] mx-auto bg-gradient-to-r from-brand-primary to-brand-secondary100">
+        <div className="bg-gradient-to-r from-brand-primary to-brand-secondary100">
             {/* discount texts */}
             <p className="p-5 md:py-2.5 font-figtree text-base sm:text-[22px] leading-5 md:leading-[26px] text-center">
                 <span className="text-brand-secondary100 font-extrabold">🚀 FRESH BEGINNINGS SALE:</span>
